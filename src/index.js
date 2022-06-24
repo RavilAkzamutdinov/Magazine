c56a62cf4f769d5affebd83762df31444fcd2ffa
Ravil <akzamutdinov1997@mail.ru>
2022-06-24 18:46:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './scss/components/_all.scss';
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.Strict>
+    <App />
+  </React.Strict>
+);
